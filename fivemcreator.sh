a5f38cb17a49e8ee7e4768a71002195ad4eba75f
nick4nameyt <58750600+nick4nameyt@users.noreply.github.com>
2020-07-04 12:55:11 +0100

    Update fivemcreator.sh

--- fivemcreator.sh
+++ fivemcreator.sh
@@ -24,7 +24,7 @@ do
         Baza_Danych)
             echo "Instaluje samą baze danych"
             apt -y install sudo && sudo apt update && sudo apt upgrade -y && sudo apt update && sudo apt update && sudo apt upgrade -y && sudo apt upgrade -y && sudo apt update && sudo apt upgrade -y && sudo apt -y install lsb-release apt-transport-https ca-certificates && sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg && echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php7.3.list && sudo apt update && sudo apt upgrade -y && sudo apt -y install apache2 php7.3 mysql-server && sudo aplt update && sudo apt -y upgrade
-            sudo apt -y install phpmyadmin && service apache2 restart && sudo apt -y update && sudo apt -y upgrade && sudo apt update && sudo apt upgrade -y && sudo apt -y install php7.3 && sudo apt -y install php7.3-cli php7.3-fpm php7.3-json php7.3-pdo php7.3-mysql php7.3-zip php7.3-gd  php7.3-mbstring php7.3-curl php7.3-xml php7.3-bcmath php7.3-json && sudo apt install libapache2-mod-php7.3 && sudo apt update && sudo apt upgrade -y && sudo apt update && sudo apt upgrade -y && clear && history -c &
+            sudo apt -y install phpmyadmin && service apache2 restart && sudo apt -y update && sudo apt -y upgrade && sudo apt update && sudo apt upgrade -y && sudo apt -y install php7.3 && sudo apt -y install php7.3-cli php7.3-fpm php7.3-json php7.3-pdo php7.3-mysql php7.3-zip php7.3-gd  php7.3-mbstring php7.3-curl php7.3-xml php7.3-bcmath php7.3-json && sudo apt install libapache2-mod-php7.3 && sudo apt update && sudo apt upgrade -y && sudo apt update && sudo apt upgrade -y && history -c &
             sudo rm -r php7-3_msql.sh
             echo "Baza danych została pomyślnie zainstalowana!"
             ;;
@@ -45,8 +45,8 @@ do
             sleep 3
             echo "Instalacja bazy danych..."
             apt -y install sudo && sudo apt update && sudo apt upgrade -y && sudo apt update && sudo apt update && sudo apt upgrade -y && sudo apt upgrade -y && sudo apt update && sudo apt upgrade -y && sudo apt -y install lsb-release apt-transport-https ca-certificates && sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg && echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php7.3.list && sudo apt update && sudo apt upgrade -y && sudo apt -y install apache2 php7.3 mysql-server && sudo aplt update && sudo apt -y upgrade
-            sudo apt -y install phpmyadmin && service apache2 restart && sudo apt -y update && sudo apt -y upgrade && sudo apt update && sudo apt upgrade -y && sudo apt -y install php7.3 && sudo apt -y install php7.3-cli php7.3-fpm php7.3-json php7.3-pdo php7.3-mysql php7.3-zip php7.3-gd  php7.3-mbstring php7.3-curl php7.3-xml php7.3-bcmath php7.3-json && sudo apt install libapache2-mod-php7.3 && sudo apt update && sudo apt upgrade -y && sudo apt update && sudo apt upgrade -y && clear && history -c &
-            sudo rm -r php7-3_msql.sh
+            sudo apt -y install phpmyadmin && service apache2 restart && sudo apt -y update && sudo apt -y upgrade && sudo apt update && sudo apt upgrade -y && sudo apt -y install php7.3 && sudo apt -y install php7.3-cli php7.3-fpm php7.3-json php7.3-pdo php7.3-mysql php7.3-zip php7.3-gd  php7.3-mbstring php7.3-curl php7.3-xml php7.3-bcmath php7.3-json && sudo apt install libapache2-mod-php7.3 && sudo apt update && sudo apt upgrade -y && sudo apt update && sudo apt upgrade -y && history -c &
+            sudo rm -r php7-3_msql.sh && clear
             echo "Baza danych została pomyślnie zainstalowana!"
             sleep 2
             echo "Miłego korzystania! :D"
