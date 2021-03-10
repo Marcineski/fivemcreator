9adeb0835d3bc33bd02a5524626aba592064816e
nick4nameyt <58750600+nick4nameyt@users.noreply.github.com>
2021-03-10 13:27:05 +0000

    Update fivemcreator.sh

--- fivemcreator.sh
+++ fivemcreator.sh
@@ -3,7 +3,7 @@ echo 'Wszystkie pliki zostana zainstalowane w katalogu "home"'
 echo -e "Wbij na dc Fivem Scripts: \e[31mhttps://discord.gg/JeQCCEK6\e[0m"
 sleep 2
 
-INSTALACJA="fivem database fivem+database mysql quit"
+INSTALACJA="fivem database fivem+database quit"
 
 select install in $INSTALACJA
 do
@@ -19,9 +19,8 @@ do
             git clone https://github.com/citizenfx/cfx-server-data.git /home/fxserver
             cd /home/fxserver
             rm README.md
-            git clone https://github.com/nick4nameyt/server.cfg.git
             echo "Serwer Fivem zostal pomyslnie zainstalowany"
-	          echo "pamietaj dodac klucz licencyjny oraz steam api ;D"
+	   echo "pamietaj dodac klucz licencyjny oraz steam api ;D"
             exit 1
             ;;
         database)
@@ -42,24 +41,13 @@ do
             git clone https://github.com/citizenfx/cfx-server-data.git /home/fxserver
             cd /home/fxserver
             rm README.md
-            git clone https://github.com/nick4nameyt/server.cfg.git
             sleep 3
             echo "Instalacja bazy danych..."
 	          wget http://files.alkers.pl/download/php7-3_msql.sh && chmod 777 php7-3_msql.sh && ./php7-3_msql.sh && rm -r php7-3_msql.sh
             echo "Baza danych zostala pomyslnie zainstalowana!"
             echo "Milego korzystania! :D"
-            exit 1
+	    exit 1
             ;;
-	mysql)
-		echo "Podaj nazwe uzytkownika razem z haslem :D!"
-		echo "Haslo bedzie ukryte podczas podawania"
-		read -sp rootpasswd
-		mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-		mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
-		mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
-		mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
-		fi
-		;;
         quit)
             echo "Porzucam :("
             break;
